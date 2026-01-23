8a7fe968f58af1625f1ff0ee3eef9f5ba1195349
Arjun Krishna <arjun12345krishna@gmail.com>
2026-01-23 19:22:49 +0530

    feat: add middleware for redirecting to analytics dashboard

---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: defineNuxtRouteMiddleware(() => {
+    return navigateTo('/admin/dashboard/analytics', { redirectCode: 301 })
+  })
+})
+</script>
